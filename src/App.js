91964ad87168a7b85517454c40b72874ce9ae2de
Chaman Modi <59691163+modi295@users.noreply.github.com>
2025-08-07 14:24:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Mr. Modi
+          Hello World
         </a>
       </header>
     </div>
